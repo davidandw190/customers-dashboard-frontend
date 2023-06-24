3f01c9be72d27a448cac9b18bae275ee6a383f3c
Andrei-David Nan <nandavid190@gmail.com>
2023-06-24 21:24:47 +0300

    Fully mapped and incorporated the `page` response object;

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {ApiResponse} from "../interface/api-response";
+import {Page} from "../interface/page";
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +14,12 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   //// NON-REACTIVE APPROACH
-  // getUser(name: string = '', page: number = 0, size: number = 0): Observable<any> {
-  //   return this.http.get<any>(`${this.serverUrl}/users?name=${name}&page=${page}&size=${size}`)
+  // getUser(name: string = '', page.ts: number = 0, size: number = 0): Observable<any> {
+  //   return this.http.get<any>(`${this.serverUrl}/users?name=${name}&page.ts=${page.ts}&size=${size}`)
   // }
 
-  users$ = (name: string = '', page: number = 0, size: number = 10): Observable<any> =>
-    this.http.get<any>(`${this.serverUrl}/users?name=${name}&page=${page}&size=${size}`);
+  users$ = (name: string = '', page: number = 0, size: number = 10): Observable<ApiResponse<Page>> =>
+    this.http.get<ApiResponse<Page>>(`${this.serverUrl}/users?name=${name}&page=${page}&size=${size}`);
 
 }
 
