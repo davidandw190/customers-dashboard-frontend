6d999ff52e15d1854b12598419826c3e7845a5f5
Andrei-David Nan <nandavid190@gmail.com>
2023-06-26 18:16:53 +0300

    Fixed the CORS config and errors in html file;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {catchError, map, Observable, of, startWith} from "rxjs";
+import {ApiResponse} from "./interface/api-response";
+import {Page} from "./interface/page";
+import {HttpErrorResponse} from "@angular/common/http";
+import {UserService} from "./service/user.service";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'customers-dash-app';
+  usersState$: Observable<{ appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse }> | undefined
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.usersState$ = this.userService.users$().pipe(
+      map((response: ApiResponse<Page>) => {
+        console.log(response);
+        return ({appState: 'APP_LOADED', appData: response});
+      }),
+      startWith({appState: 'APP_LOADING'}),
+      catchError((error: HttpErrorResponse) => of({appState: 'APP_ERROR', error}))
+    )
+  }
 }
