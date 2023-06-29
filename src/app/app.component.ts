4c367546c809c0315f405d1caab0ef44d0dcef09
Andrei-David Nan <nandavid190@gmail.com>
2023-06-29 21:09:03 +0300

    Created `goToPage` func and found a way to store the application data from last page until the observable gets the new data;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {catchError, map, Observable, of, startWith} from "rxjs";
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
 import {ApiResponse} from "./interface/api-response";
 import {Page} from "./interface/page";
 import {HttpErrorResponse} from "@angular/common/http";
@@ -14,16 +14,36 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
 export class AppComponent {
   title = 'customers-dash-app';
   usersState$: Observable<{ appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse }> | undefined
+  responseSubject = new BehaviorSubject<ApiResponse<Page>>(null);
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.usersState$ = this.userService.users$().pipe(
       map((response: ApiResponse<Page>) => {
+        this.responseSubject.next(response);
+        console.log(response);
+        return ({
+          appState: 'APP_LOADED',
+          appData: response
+        });
+      }),
+      startWith({appState: 'APP_LOADING'}),
+      catchError((error: HttpErrorResponse) => of({appState: 'APP_ERROR', error}))
+    )
+  }
+
+  goToPage(name?: string, pageNumber?: number): void {
+    this.usersState$ = this.userService.users$(name, pageNumber).pipe(
+      map((response: ApiResponse<Page>) => {
+        this.responseSubject.next(response);
         console.log(response);
         return ({appState: 'APP_LOADED', appData: response});
       }),
-      startWith({appState: 'APP_LOADING'}),
+      startWith({
+        appState: 'APP_LOADED',
+        appData: this.responseSubject.value
+      }),
       catchError((error: HttpErrorResponse) => of({appState: 'APP_ERROR', error}))
     )
   }
